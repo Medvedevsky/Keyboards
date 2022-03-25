50d956608f5d2d8be2b316930ee447f3ffcca1c9
stillProger <61091044+stillProger@users.noreply.github.com>
2022-03-25 14:55:49 +0300

    feat: add user control in main window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApp
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            textBox.Text = ruKeyboard.KeyboardInput;
+        }
     }
 }
