002964ff43e6a4902945d26e2dab990da8f9c120
stillProger <61091044+stillProger@users.noreply.github.com>
2022-03-28 15:44:48 +0300

    feat: symbol keyboard

--- Components/SymbolKeyboard.xaml.cs
+++ Components/SymbolKeyboard.xaml.cs
@@ -20,9 +20,48 @@ namespace WpfApp.Components
     /// </summary>
     public partial class SymbolKeyboard : UserControl
     {
+
+        private bool changeKeyboard = true;
+        public string SymbolKeyboardInput
+        {
+            get { return (string)GetValue(SymbolKeyboardInputProperty); }
+            set { SetValue(SymbolKeyboardInputProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SymbolKeyboardInput.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SymbolKeyboardInputProperty =
+            DependencyProperty.Register("SymbolKeyboardInput", typeof(string), typeof(SymbolKeyboard), new PropertyMetadata(""));
+
+
         public SymbolKeyboard()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click_Key(object sender, RoutedEventArgs e)
+        {
+            //SymbolKeyboardInput += ((TextBlock)((Viewbox)((Button)sender).Content).Child).Text;
+
+            Button buttonKey = (Button)sender;
+            Viewbox viewBox = (Viewbox)buttonKey.Content;
+            TextBlock keyContent = (TextBlock)viewBox.Child;
+            SymbolKeyboardInput += keyContent.Text;
+        }
+
+        private void Button_Click_ChangeKeyboard(object sender, RoutedEventArgs e)
+        {
+            changeKeyboard = true;
+        }
+
+        private void Button_Click_Delete(object sender, RoutedEventArgs e)
+        {
+            int countInput = SymbolKeyboardInput.Length - 1;
+            SymbolKeyboardInput = countInput >= 0 ? SymbolKeyboardInput.Remove(countInput) : SymbolKeyboardInput;
+        }
+
+        private void Button_Click_Space(object sender, RoutedEventArgs e)
+        {
+            SymbolKeyboardInput += " ";
         }
     }
 }
