a7a38969214ae5c20f0f43f4c41449ea3b0bde3f
stillProger <61091044+stillProger@users.noreply.github.com>
2022-04-06 16:41:16 +0300

    feat: add enum KeyboardType

---
+++ Components/Enums/KeyboardType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Components.Enums
+{
+    public enum KeyboardType
+    {
+        KeyboardRu = 0,
+        SymbolsKeyboard,
+        KeyboardEng,
+        KeyboardNumeric
+    }
+}
