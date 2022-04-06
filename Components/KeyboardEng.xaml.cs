ba26f03da084cd44ee90f1e1ee10d9e730f8f495
stillProger <61091044+stillProger@users.noreply.github.com>
2022-04-06 09:41:42 +0300

    feat: add eng keyboard

--- Components/KeyboardEng.xaml.cs
+++ Components/KeyboardEng.xaml.cs
@@ -20,9 +20,93 @@ namespace WpfApp.Components
     /// </summary>
     public partial class KeyboardEng : UserControl
     {
+        private bool isCapsLock = false;
         public KeyboardEng()
         {
             InitializeComponent();
+        }
+
+
+        public ICommand SwitchToRuKeyboard
+        {
+            get { return (ICommand)GetValue(SwitchToRuKeyboardProperty); }
+            set { SetValue(SwitchToRuKeyboardProperty, value); }
+        }
+
+
+        public ICommand SwitchToSymbolKeyboard
+        {
+            get { return (ICommand)GetValue(SwitchToSymbolKeyboardProperty); }
+            set { SetValue(SwitchToSymbolKeyboardProperty, value); }
+        }
+
+
+        public string KeyboardEngInput
+        {
+            get { return (string)GetValue(KeyboardEngInputProperty); }
+            set { SetValue(KeyboardEngInputProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty SwitchToRuKeyboardProperty =
+            DependencyProperty.Register(
+                "SwitchToRuKeyboard", 
+                typeof(ICommand), 
+                typeof(KeyboardEng), 
+                new PropertyMetadata(null));
+
+
+        public static readonly DependencyProperty SwitchToSymbolKeyboardProperty =
+            DependencyProperty.Register(
+                "SwitchToSymbolKeyboard", 
+                typeof(ICommand), 
+                typeof(KeyboardEng), 
+                new PropertyMetadata(null));
+
+
+        public static readonly DependencyProperty KeyboardEngInputProperty =
+            DependencyProperty.Register(
+                "KeyboardEngInput",
+                typeof(string),
+                typeof(KeyboardEng),
+                new PropertyMetadata(""));
+
+        private void Button_Click_Key(object sender, RoutedEventArgs e)
+        {
+            Button buttonKey = (Button)sender;
+            Viewbox viewBox = (Viewbox)buttonKey.Content;
+            TextBlock keyContent = (TextBlock)viewBox.Child;
+            KeyboardEngInput += keyContent.Text;
+        }
+
+        private void Button_Click_CapsLock(object sender, RoutedEventArgs e)
+        {
+
+            isCapsLock = !isCapsLock;
+            List<Grid> gridsList = mainGrid.Children.OfType<Grid>().ToList();
+
+            foreach (Grid grid in gridsList)
+            {
+                List<Button> buttonList = grid.Children.OfType<Button>().ToList();
+
+                foreach (Button buttonKey in buttonList)
+                {
+                    Viewbox viewBox = (Viewbox)buttonKey.Content;
+                    TextBlock keyContent = (TextBlock)viewBox.Child;
+
+                    keyContent.Text = isCapsLock == true ? keyContent.Text.ToUpper() : keyContent.Text.ToLower();
+                }
+            }
+        }
+        private void Button_Click_Delete(object sender, RoutedEventArgs e)
+        {
+            int countInput = KeyboardEngInput.Length - 1;
+            KeyboardEngInput = countInput >= 0 ? KeyboardEngInput.Remove(countInput) : KeyboardEngInput;
+        }
+
+        private void Button_Click_Space(object sender, RoutedEventArgs e)
+        {
+            KeyboardEngInput += " ";
         }
     }
 }
