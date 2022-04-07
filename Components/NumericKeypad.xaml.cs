1d5cdc94b57d6eac6c2c8273a8e515738856030b
stillProger <61091044+stillProger@users.noreply.github.com>
2022-04-07 09:08:41 +0300

    feat: Add numeric keyboard

--- Components/NumericKeypad.xaml.cs
+++ Components/NumericKeypad.xaml.cs
@@ -24,5 +24,34 @@ namespace WpfApp.Components
         {
             InitializeComponent();
         }
+
+        public string KeyboardNumericInput
+        {
+            get { return (string)GetValue(KeyboardNumericInputProperty); }
+            set { SetValue(KeyboardNumericInputProperty, value); }
+        }
+
+       
+        public static readonly DependencyProperty KeyboardNumericInputProperty =
+            DependencyProperty.Register(
+                "KeyboardNumericInput", 
+                typeof(string), 
+                typeof(NumericKeypad), 
+                new PropertyMetadata(0));
+
+
+        private void Button_Click_Key(object sender, RoutedEventArgs e)
+        {
+            Button buttonKey = (Button)sender;
+            Viewbox viewBox = (Viewbox)buttonKey.Content;
+            TextBlock keyContent = (TextBlock)viewBox.Child;
+            KeyboardNumericInput += keyContent.Text;
+        }
+
+        private void Button_Click_Delete(object sender, RoutedEventArgs e)
+        {
+            int position = KeyboardNumericInput.Length - 1;
+            KeyboardNumericInput = position >= 0 ? KeyboardNumericInput.Remove(position) : KeyboardNumericInput;
+        }
     }
 }
