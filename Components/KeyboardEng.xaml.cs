3c0b4a607dbafb743d654e69d779e2a78a6c86eb
stillProger <61091044+stillProger@users.noreply.github.com>
2022-04-08 15:12:36 +0300

    refactor

--- Components/KeyboardEng.xaml.cs
+++ Components/KeyboardEng.xaml.cs
@@ -1,23 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp.Components
 {
-    /// <summary>
-    /// Interaction logic for KeyboardEng.xaml
-    /// </summary>
     public partial class KeyboardEng : UserControl
     {
         private bool isCapsLock = false;
@@ -25,7 +13,6 @@ namespace WpfApp.Components
         {
             InitializeComponent();
         }
-
 
         public ICommand SwitchToRuKeyboard
         {
--- Components/KeyboardsSwitcher.xaml.cs
+++ Components/KeyboardsSwitcher.xaml.cs
@@ -1,25 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WpfApp.Components.Enums;
 
 namespace WpfApp.Components
 {
     public sealed class MyCommand : ICommand
     {
-        
-
         private readonly Action _execute;
 
         public MyCommand(Action execute)
@@ -52,8 +40,6 @@ namespace WpfApp.Components
             SwitchSymbols = new MyCommand(() => SwitchToSymbolKeyboard());
             SwitchKeyboard = new MyCommand(() => SwitchLanguage());
         }
-
-
 
         public ICommand SwitchKeyboard
         {
